477a627ed8ea2163b69d8ab18c04b432cfe53ff6
Harshal Thaware <htpractice8@gmail.com>
2025-03-21 11:53:43 +0530

    updated output for server ecr

--- project/terraform/infra/output.tf
+++ project/terraform/infra/output.tf
@@ -9,11 +9,16 @@ output "jenkins_app_repo_url" {
   value = aws_ecr_repository.jenkins_app_repo.repository_url
 }
 
-# Output the movie repository URL
-output "movie_app_repo_url" {
-  value = aws_ecr_repository.movie_app_repo.repository_url
+# Output the movie client repository URL
+output "movie_app_client_repo_url" {
+  value = aws_ecr_repository.movie_app_client_repo.repository_url
 }
 
+# Output the movie server repository URL
+output "movie_app_server_repo_url" {
+  value = aws_ecr_repository.movie_app_server_repo.repository_url
+  
+}
 #Output for the bastion host
 output "bastion_host" {
   value = module.bastion.public_ip
