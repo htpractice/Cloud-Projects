5f9780abaae15fc5636b3c30deb0020354067121
Harshal Thaware <htpractice8@gmail.com>
2025-03-21 10:53:07 +0530

    Created a common role for app,jenkins,bastion/ansible since all need access to ecr and ec2

--- project/terraform/infra/iam.tf
+++ project/terraform/infra/iam.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "bastion_role" {
-  name = "${var.environment}-bastion-role"
+resource "aws_iam_role" "common_role" {
+  name = "${var.environment}-common-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -15,58 +15,10 @@ resource "aws_iam_role" "bastion_role" {
   })
 }
 
-resource "aws_iam_policy" "bastion_read_only_policy" {
-  name        = "${var.environment}-bastion-read-only-policy"
-  description = "Read-only policy for bastion host to access EC2 resources"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "ec2:Describe*",
-          "ec2:Get*",
-          "ec2:List*"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "bastion_policy_attachment" {
-  role       = aws_iam_role.bastion_role.name
-  policy_arn = aws_iam_policy.bastion_read_only_policy.arn
-}
-
-resource "aws_iam_instance_profile" "bastion_instance_profile" {
-  name = "${var.environment}-bastion-instance-profile"
-  role = aws_iam_role.bastion_role.name
-}
-
-# IAM Role for Jenkins and App Nodes
-resource "aws_iam_role" "jenkins_app_role" {
-  name = "${var.environment}-jenkins-app-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-# IAM Policy for ECR Access
-resource "aws_iam_policy" "ecr_policy" {
-  name        = "${var.environment}-ecr-policy"
-  description = "Policy for Jenkins and app nodes to access ECR"
+# IAM Policy for ECR and EC2 Access
+resource "aws_iam_policy" "ecr_ec2_policy" {
+  name        = "${var.environment}-ecr-ec2-policy"
+  description = "Policy for instances to access ECR and EC2 resources"
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -104,12 +56,12 @@ resource "aws_iam_policy" "ecr_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "jenkins_app_policy_attachment" {
-  role       = aws_iam_role.jenkins_app_role.name
-  policy_arn = aws_iam_policy.ecr_policy.arn
+resource "aws_iam_role_policy_attachment" "common_policy_attachment" {
+  role       = aws_iam_role.common_role.name
+  policy_arn = aws_iam_policy.ecr_ec2_policy.arn
 }
 
-resource "aws_iam_instance_profile" "jenkins_app_instance_profile" {
-  name = "${var.environment}-jenkins-app-instance-profile"
-  role = aws_iam_role.jenkins_app_role.name
+resource "aws_iam_instance_profile" "common_instance_profile" {
+  name = "${var.environment}-common-instance-profile"
+  role = aws_iam_role.common_role.name
 }
