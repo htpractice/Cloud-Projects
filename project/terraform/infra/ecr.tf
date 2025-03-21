3ec0db7f5dc31d46c9d5964136270c6307b6cab5
Harshal Thaware <htpractice8@gmail.com>
2025-03-21 11:53:25 +0530

    added movie client and server repo

--- project/terraform/infra/ecr.tf
+++ project/terraform/infra/ecr.tf
@@ -15,9 +15,9 @@ resource "aws_ecr_repository" "jenkins_app_repo" {
   }
 }
 
-# Movie App Image repository
-resource "aws_ecr_repository" "movie_app_repo" {
-  name = "${var.environment}-movie-app-repo"
+# Movie App Client Image repository
+resource "aws_ecr_repository" "movie_app_client_repo" {
+  name = "${var.environment}-movie-app-client-repo"
 
   image_scanning_configuration {
     scan_on_push = true
@@ -26,7 +26,23 @@ resource "aws_ecr_repository" "movie_app_repo" {
   image_tag_mutability = "MUTABLE"
 
   tags = {
-    Name        = "${var.environment}-movie-app-repo"
+    Name        = "${var.environment}-movie-app-client-repo"
+    Environment = var.environment
+  }
+}
+
+# Movie App Server Image repository
+resource "aws_ecr_repository" "movie_app_server_repo" {
+  name = "${var.environment}-movie-app-server-repo"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  image_tag_mutability = "MUTABLE"
+
+  tags = {
+    Name        = "${var.environment}-movie-app-server-repo"
     Environment = var.environment
   }
 }
