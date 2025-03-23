b40d18c705b242a505ff88f7e1a4fbec344c0cf5
Harshal Thaware <htpractice8@gmail.com>
2025-03-23 10:14:02 +0530

    updated script to be executable

--- project/docker/build.sh
+++ project/docker/build.sh
@@ -3,7 +3,7 @@
 # Define the list of arguments
 REGION="$1"
 AWS_ACCOUNT_ID="$2"
-AWS_REPO_NAMES=("$3","$4")
+AWS_REPO_NAMES=("$3" "$4")
 
 # Build the Docker image with the arguments
 docker build --build-arg REGION=$REGION --build-arg AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID --build-arg AWS_REPO_NAMES="${AWS_REPO_NAMES[*]}" -t jenkins-docker:latest .
