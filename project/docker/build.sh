34ed534dee50024b52c43a03e35aad0a3567456b
Harshal Thaware <harshal.thaware@autodesk.com>
2025-04-30 11:18:26 +0530

    removed project files

--- project/docker/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Define the list of arguments
-REGION="$1"
-AWS_ACCOUNT_ID="$2"
-AWS_REPO_NAMES=("$3" "$4")
-
-# Build the Docker image with the arguments
-docker build --build-arg REGION=$REGION --build-arg AWS_ACCOUNT_ID=$AWS_ACCOUNT_ID --build-arg AWS_REPO_NAMES="${AWS_REPO_NAMES[*]}" -t jenkins-docker:latest .
